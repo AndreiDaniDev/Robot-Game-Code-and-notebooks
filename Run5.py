cc0c71712aeb6c207d083e11a5bddc9f4a0f291c
AndreiDani <89001893+AndreiIPIT@users.noreply.github.com>
2024-06-04 19:16:16 +0300

    Update Run5.py

--- Run5.py
+++ Run5.py
@@ -1,11 +1,20 @@
-#be aware that this was made in Spike Prime 3.4.0
+#be aware that this was made in Spike Prime 3.4.3
 import runloop, hub, motor, motor_pair, color
+
+
+# ---> Robot Ports <---
+leftDriveBaseMotor = hub.port.B
+rightDriveBaseMotor = hub.port.D
+leftSystemMotor = hub.port.A
+rightSystemMotor = hub.port.C
+#leftColorSensor = NULL
+#rightColorSensor = NULL
 
 async def gyroForward(distance: float, speed: int, target: int = 0, * , kp: float = 0.25, kd: float = 0.3, stop: bool = True, reset: bool = True):
     d = int(distance * 20.45); lastError = 0
     if(reset): hub.motion_sensor.reset_yaw(0)
-    motor.reset_relative_position(hub.port.B, 0)
-    while(abs(motor.relative_position(hub.port.B)) < d):
+    motor.reset_relative_position(leftDriveBaseMotor, 0)
+    while(abs(motor.relative_position(leftDriveBaseMotor)) < d):
         error = (hub.motion_sensor.tilt_angles()[0] - target) / 10
         proportional = error * -kp
         differential = (error - lastError) * -kd
@@ -16,8 +25,8 @@ async def gyroForward(distance: float, speed: int, target: int = 0, * , kp: floa
 async def gyroBackwards(distance: float, speed: int, target: int = 0, * , kp: float = 0.25, kd: float = 0.3 , stop: bool = True, reset: bool = True):
     d = int(distance * 20.45); lastError = 0; speed *= -1
     if(reset): hub.motion_sensor.reset_yaw(0)
-    motor.reset_relative_position(hub.port.B, 0)
-    while(abs(motor.relative_position(hub.port.B)) < d):
+    motor.reset_relative_position(leftDriveBaseMotor, 0)
+    while(abs(motor.relative_position(leftDriveBaseMotor)) < d):
         error = (hub.motion_sensor.tilt_angles()[0] - target) / 10
         proportional = error * kp
         differential = (error - lastError) * kd
@@ -37,7 +46,7 @@ async def turnLeft(grade: float, speedMax: int = 500, speedMin: int = 150, * , r
         if(abs(output) > speedMax): output = speedMax
         if(abs(output) < speedMin): output = speedMin
         motor_pair.move_tank(motor_pair.PAIR_1, -int(output), int(output))
-    motor_pair.stop(motor_pair.PAIR_1)
+    motor_pair.stop(motor_pair.PAIR_1, stop=motor.SMART_BRAKE)
 
 async def turnRight(grade: float, speedMax: int = 500, speedMin: int = 150, * , reset: bool = True, kp: float = 0.25, kd: float = 0.35):
     if(reset): hub.motion_sensor.reset_yaw(0)
@@ -51,7 +60,7 @@ async def turnRight(grade: float, speedMax: int = 500, speedMin: int = 150, * , 
         if(abs(output) > speedMax): output = speedMax
         if(abs(output) < speedMin): output = speedMin
         motor_pair.move_tank(motor_pair.PAIR_1, int(output), -int(output))
-    motor_pair.stop(motor_pair.PAIR_1)
+    motor_pair.stop(motor_pair.PAIR_1, stop=motor.SMART_BRAKE)
 
 async def turnLeftOneWheel(grade: float, speedMax: int = 500, speedMin: int = 150, * , reset: bool = True, kp: float = 0.25, kd: float = 0.35):
     if(reset): hub.motion_sensor.reset_yaw(0)
@@ -65,7 +74,7 @@ async def turnLeftOneWheel(grade: float, speedMax: int = 500, speedMin: int = 15
         if(abs(output) > speedMax): output = speedMax
         if(abs(output) < speedMin): output = speedMin
         motor_pair.move_tank(motor_pair.PAIR_1, 0, int(output))
-    motor_pair.stop(motor_pair.PAIR_1)
+    motor_pair.stop(motor_pair.PAIR_1, stop=motor.SMART_BRAKE)
 
 async def turnRightOneWheel(grade: float, speedMax: int = 500, speedMin: int = 150, * , reset: bool = True, kp: float = 0.25, kd: float = 0.35):
     if(reset): hub.motion_sensor.reset_yaw(0)
@@ -79,7 +88,7 @@ async def turnRightOneWheel(grade: float, speedMax: int = 500, speedMin: int = 1
         if(abs(output) > speedMax): output = speedMax
         if(abs(output) < speedMin): output = speedMin
         motor_pair.move_tank(motor_pair.PAIR_1, int(output), 0)
-    motor_pair.stop(motor_pair.PAIR_1)
+    motor_pair.stop(motor_pair.PAIR_1, stop=motor.SMART_BRAKE)
 
 async def turnLeftOtherWheel(grade: float, speedMax: int = 500, speedMin: int = 150, * , reset: bool = True, kp: float = 0.25, kd: float = 0.35):
     if(reset): hub.motion_sensor.reset_yaw(0)
@@ -93,7 +102,7 @@ async def turnLeftOtherWheel(grade: float, speedMax: int = 500, speedMin: int = 
         if(abs(output) > speedMax): output = speedMax
         if(abs(output) < speedMin): output = speedMin
         motor_pair.move_tank(motor_pair.PAIR_1, -int(output), 0)
-    motor_pair.stop(motor_pair.PAIR_1)
+    motor_pair.stop(motor_pair.PAIR_1, stop=motor.SMART_BRAKE)
 
 async def turnRightOtherWheel(grade: float, speedMax: int = 500, speedMin: int = 150, * , reset: bool = True, kp: float = 0.25, kd: float = 0.35):
     if(reset): hub.motion_sensor.reset_yaw(0)
@@ -107,52 +116,50 @@ async def turnRightOtherWheel(grade: float, speedMax: int = 500, speedMin: int =
         if(abs(output) > speedMax): output = speedMax
         if(abs(output) < speedMin): output = speedMin
         motor_pair.move_tank(motor_pair.PAIR_1, 0, -int(output))
-    motor_pair.stop(motor_pair.PAIR_1)
+    motor_pair.stop(motor_pair.PAIR_1, stop=motor.SMART_BRAKE)
 
 async def run5_WPI():
     #---> You can modify after this <---
-    print("Run 5")
 
+    # ---> pull-back car <---
     hub.motion_sensor.reset_yaw(0)
-    await motor.run_for_degrees(hub.port.C, 300, -1100)
+    await motor.run_for_degrees(leftSystemMotor, 300, -1100)
     await runloop.sleep_ms(500)
+    
     # ---> Getting in position <---
-    await gyroForward(45, 600, kp = 0.4, kd = 0.25, reset = False)
+    await gyroBackwards(0.5, 250, kp = 0.4, kd = 0.25)
+    await gyroForward(44, 600, kp = 0.4, kd = 0.25, reset = False)
     await runloop.sleep_ms(250)
-    await turnLeft(42.5, 650, 125, kp = 0.75, kd = 1)
+    await turnLeft(44, 650, 125, kp = 0.75, kd = 1)
     await runloop.sleep_ms(250)
 
     # ---> Slowing down <---
-    await gyroForward(5.5, 400, kp = 0.3, kd = 0.25, stop = False)
-    await gyroForward(14.5, 500, kp = 0.3, kd = 0.25, reset = False, stop = False)
+    await gyroForward(6.5, 400, kp = 0.3, kd = 0.25, stop = False)
+    await gyroForward(14.25, 500, target=30, kp = 0.3, kd = 0.25, reset = False, stop = False)
     await gyroForward(5, 250, kp = 0.3, kd = 0.25, reset = False)
 
     # ---> The marge of error <---
     await runloop.sleep_ms(500)
-    await turnRight(77.5, 500, 175, kp = 0.75, kd = 1.2, reset = False)
+    await turnRight(87, 500, 175, kp = 0.75, kd = 1.2, reset = False)
 
     # ---> Dummy proof <---
-    #    ->>> 20 lei <<<-
-    await motor.run_for_degrees(hub.port.C, 300, 1100)
+    await motor.run_for_degrees(leftSystemMotor, 300, 1100)
     await runloop.sleep_ms(250)
-    await gyroForward(14, 500, target = 35, kp = 0.4, kd = 0.25)
-    await gyroForward(10, 800, kp = 0.4, kd = 0.25)
+    await gyroForward(14, 500, target = 20, kp = 0.4, kd = 0.25)
+    await gyroForward(10, 750, kp = 0.4, kd = 0.25)
     await runloop.sleep_ms(250)
-    await gyroBackwards(14.5, 500, kp = 0.4, kd = 0.25)
 
+    # ---> Returning to the base <---
+    await gyroBackwards(14.5, 500, kp = 0.4, kd = 0.25)
     await turnLeft(60, 500, 250, kp = 0.4, kd = 0.25)
     await gyroBackwards(75, 1000, kp = 0.4, kd = 0.25)
 
     #---> You can't modify after this <---
     return
 
-def play():
-    return not (hub.button.pressed(hub.button.RIGHT) or hub.button.pressed(hub.button.LEFT))
-def pressPlay():
-    return (bool)(hub.button.pressed(hub.button.RIGHT) or hub.button.pressed(hub.button.LEFT))
-
+def play(): return not (hub.button.pressed(hub.button.RIGHT) or hub.button.pressed(hub.button.LEFT))
+def pressPlay(): return (bool)(hub.button.pressed(hub.button.RIGHT) or hub.button.pressed(hub.button.LEFT))
 async def main():
-    motor_pair.pair(motor_pair.PAIR_1, hub.port.B, hub.port.D)
+    motor_pair.pair(motor_pair.PAIR_1, leftDriveBaseMotor, rightDriveBaseMotor)
     await run5_WPI()
-
 runloop.run(main())
